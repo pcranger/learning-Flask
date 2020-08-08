4b81357256924bd599eccb1f7441fac03641d6b6
pcranger <tmhieu2000@gmail.com>
2020-08-08 15:32:05 +0700

    section4

--- section4/code/app.py
+++ section4/code/app.py
@@ -1,13 +1,15 @@
-from flask import Flask 
+from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app)  # define api
 
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
 
-api.add_resouce(Student, '/student/<string:name>')
+class Student(Resource):  # define resource
+    def get(self, name):  # define method of this resource
+        return {'student': name}  # define what this method do
 
-app.run(port = 5000)
+
+# define how the path for the resource
+api.add_resource(Student, '/student/<string:name>')
+app.run(port=5000)
