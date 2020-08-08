9d901af72410fa3ce4991521fb7326bca12e18b5
pcranger <tmhieu2000@gmail.com>
2020-08-08 15:56:06 +0700

    4.68

--- section4/code/app.py
+++ section4/code/app.py
@@ -1,15 +1,25 @@
 from flask import Flask
 from flask_restful import Resource, Api
-
+# flask_restful automatically convert dict to json, so jsonify is not needed
 app = Flask(__name__)
-api = Api(app)  # define api
+api = Api(app)
+
+items = []
 
 
-class Student(Resource):  # define resource
-    def get(self, name):  # define method of this resource
-        return {'student': name}  # define what this method do
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404  # if the item name  cannot be found
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item
 
 
 # define how the path for the resource
-api.add_resource(Student, '/student/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
 app.run(port=5000)
