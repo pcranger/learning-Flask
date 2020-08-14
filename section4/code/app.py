9ee084c80f2f0b3c331873572406e32229287b60
pcranger <tmhieu2000@gmail.com>
2020-08-14 16:33:16 +0700

    4.75 parser

--- section4/code/app.py
+++ section4/code/app.py
@@ -43,13 +43,20 @@ class Item(Resource):
         return {'message': 'Item deleted'}
 
     def put(self, name):
-        data = request.get_json()
+        # prevent adding item without a price by looking at json payload
+        parser = reqparse.RequestParser()
+        parser.add_argument('price',
+                            type=float,
+                            required=True,
+                            help="This field cannot be left blank"
+                            )
+        data = parser.parse_args()
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
             items.append(item)
         else:
-            item.update(data)
+            items.update(data)
         return item
 
 
