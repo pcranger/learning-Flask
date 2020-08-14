7b47b79502452af5fe056a0d3c456d0aa6ca18ba
pcranger <tmhieu2000@gmail.com>
2020-08-14 16:38:14 +0700

    4.76 finialising

--- section4/code/app.py
+++ section4/code/app.py
@@ -20,6 +20,12 @@ items = []
 
 class Item(Resource):
     # /item/<string:name>
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank"
+                        )
+
     @jwt_required()
     def get(self, name):
         # next(filter()): return the first item found from items of filer function or none if there is no item
@@ -28,10 +34,13 @@ class Item(Resource):
         return {'item': None}, 200 if item else 404
 
     def post(self, name):
+
         # if found an item and it's not None
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': "An item with name '{}' already exists. for".format(name)}, 400
-        data = request.get_json()  # accept only json type data
+
+        data = Item.parser.parse_args()
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
@@ -43,14 +52,11 @@ class Item(Resource):
         return {'message': 'Item deleted'}
 
     def put(self, name):
+
+        data = Item.parser.parse_args()
+
         # prevent adding item without a price by looking at json payload
         parser = reqparse.RequestParser()
-        parser.add_argument('price',
-                            type=float,
-                            required=True,
-                            help="This field cannot be left blank"
-                            )
-        data = parser.parse_args()
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
