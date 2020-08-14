446ff10018779b5152d725787b6dd5dc9d38f81c
pcranger <tmhieu2000@gmail.com>
2020-08-14 16:19:09 +0700

    4.73 delete

--- section4/code/app.py
+++ section4/code/app.py
@@ -1,21 +1,26 @@
 from flask import Flask, request
 # request: filtering types of requests data
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 # flask_restful automatically convert dict to json, so jsonify is not needed
-from jlask_jwt import jlask_jwt
+from flask_jwt import JWT, jwt_required, current_identity
 
 from security import authenticate, identity
 app = Flask(__name__)
 app.secret_key = 'Hieu'
 api = Api(app)
-
+# app secrerity key(jwt token)
+# when a function is sent jwt token, the user have that token will have access to that function cause he logged in
 jwt = JWT(app, authenticate, identity)
 # jwt creates new endpoints /auth
 
 items = []
 
+# item is a resource, it can get, post
+
 
 class Item(Resource):
+    # /item/<string:name>
+    @jwt_required()
     def get(self, name):
         # next(filter()): return the first item found from items of filer function or none if there is no item
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -31,8 +36,25 @@ class Item(Resource):
         items.append(item)
         return item, 201
 
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        # create a new list without the item and overwrite the existing list with this
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
 
 class ItemList(Resource):
+    # /items
     def get(self):
         return {'item': items}
 
--- section4/code/security.py
+++ section4/code/security.py
@@ -1,22 +1,16 @@
+from werkzeug.security import safe_str_cmp
 from user import User
 users = [User(1, 'bob', '1234')]
 
 # u for u in users: List comprehension
 username_mapping = {u.username: u for u in users}
 userid_mapping = {u.id: u for u in users}
-
-userid_mapping = {1: {
-    'id': 1,
-    'username': 'bob',
-    'password': '1234'
-}
-}
 # map username with their coresponding id
 
 
 def authenticate(username, password):
     user = username_mapping.get(username, None)
-    if user and user.password == password:
+    if user and safe_str_cmp(user.password, password):
         return user
 
 # payload: content of JWT token, identify that this user with id logged in
